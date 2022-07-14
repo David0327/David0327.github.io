0cf25f392be7aa4f7ed3fbbe7504f73af1a7742c
David0327 <david.lewis@ihsnola.org>
2022-07-14 19:17:27 +0000

    Added bower components

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -1,62 +1,61 @@
 var init = function (window) {
     'use strict';
-    var 
+    var
         draw = window.opspark.draw,
         physikz = window.opspark.racket.physikz,
-        
+
         app = window.opspark.makeApp(),
-        canvas = app.canvas, 
+        canvas = app.canvas,
         view = app.view,
         fps = draw.fps('#000');
-        
-    
-    window.opspark.makeGame = function() {
-        
+
+
+    window.opspark.makeGame = function () {
+
         window.opspark.game = {};
         var game = window.opspark.game;
-        var circles; 
-        
+        var circles;
+
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM SETUP ////////////////////////////
         ////////////////////////////////////////////////////////////
-        
+
         // TODO 1 : Declare and initialize our variables
         var circle;
-        var circles = []; 
+        var circles = [];
 
         // TODO 2 : Create a function that draws a circle 
-        var drawCircle = function() {
+
         // Code to draw a circle
 
-        circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
-        physikz.addRandomVelocity(circle, canvas);
-        view.addChild(circle);
-        circles.push(circle);
-        
-        
-        
+
+        function drawCircle() {
+            circle = draw.randomCircleInArea(canvas, true, true, '#999', 2);
+            physikz.addRandomVelocity(circle, canvas);
+            view.addChild(circle);
+            circles.push(circle);
+
+        }
+
+
+
 
         // TODO 3 / 7 : Call the drawCircle() function 
 
-        drawCircle(1);
-        drawCircle(2);    
-        drawCircle(3);
-        drawCircle(4);
-        drawCircle(5);
-        
+
         var counter = 0;
         while (counter < 100) {
             drawCircle();
             counter++;
         }
 
-        
+
 
 
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
-        
+
         /* 
         This Function is called 60 times/second producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
@@ -64,54 +63,53 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-            circle.x = circle.x + circle.velocityX;
-            circle.y = circle.y + circle.velocityY;
 
-            
+
+
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+
 
             // TODO 9 : Iterate over the array
-           
-            
+
+
         }
-    
+
         /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-        game.checkCirclePosition = function(circle) {
+        game.checkCirclePosition = function (circle) {
 
             // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
-            if ( circle.x > canvas.width ) {
+            if (circle.x > canvas.width) {
                 circle.x = 0;
             }
-            
+
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
         }
-        
+
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
         /////////////////////////////////////////////////////////////
-        
+
         view.addChild(fps);
         app.addUpdateable(fps);
-        
+
         game.circle = circle;
         game.drawCircle = drawCircle;
-        
-        
+
+
         app.addUpdateable(window.opspark.game);
     }
 };
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
-if((typeof process !== 'undefined') &&
+if ((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
